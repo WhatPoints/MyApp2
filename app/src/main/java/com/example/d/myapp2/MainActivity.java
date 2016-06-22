a46857d44e108f5d5789e5844d541a2d7c90cd9f
baoxiaofeng <baoxiaofeng@gengee.cn>
2016-06-22 11:53:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/d/myapp2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.d.myapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                NewMessageNotification.notify(MainActivity.this, "ticker", 2);
+            }
+
+        });
+
+    }
+
+}
